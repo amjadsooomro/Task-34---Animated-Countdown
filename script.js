f594c6db751562bb345d39099c56e31f521079ca
amjadaliSoomro <76951683+amjadsooomro@users.noreply.github.com>
2021-09-07 16:14:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var nums = document.querySelectorAll('.nums span')
+var counter = document.querySelector('.counter')
+var finalMessage = document.querySelector('.final')
+var replay = document.querySelector('#replay')
+
+
+runAnimation()
+
+function resetDom(){
+    counter.classList.remove('hide')
+    finalMessage.classList.remove('show')
+
+    nums.forEach((num) => {
+        num.classList.value = ''
+    })
+
+    nums[0].classList.add('in')
+}
+
+function runAnimation(){
+    nums.forEach((num , idx) => {
+        var nextToLast = nums.length - 1
+
+        num.addEventListener('animationend', (e) =>{
+            if(e.animationName === 'goIn' && idx !== nextToLast){
+                num.classList.remove('in')
+                num.classList.add('out')
+            }else if(e.animationName === 'goOut' && num.nextElementSibling){
+                num.nextElementSibling.classList.add('in')
+            }else{
+                counter.classList.add('hide')
+                finalMessage.classList.add('show')
+            }
+        })
+    })
+}
+
+replay.addEventListener('click', () => {
+    resetDom()
+    runAnimation()
+})
